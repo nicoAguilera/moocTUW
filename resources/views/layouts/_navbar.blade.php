17c2725673ab7cddf60d01658c9d7bc0def9d4d9
Nicolas Aguilera <beltrusena@gmail.com>
2016-06-02 23:55:12 -0400

    Unificando frontend a materializecss

--- resources/views/layouts/_navbar.blade.php
+++ resources/views/layouts/_navbar.blade.php
@@ -1,33 +1,60 @@
-<nav class="navbar navbar-dark indigo darken-2">
-	<button type="button" class="navbar-toggler hidden-sm-up" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-		<i class="fa fa-bars"></i>
-	</button>
-	<div class="container">
-		<div class="collapse navbar-toggleable-xs" id="bs-example-navbar-collapse-1">
-			<a class="navbar-brand" href="{{ URL::route('home') }}">{{ $app_name }}</a>
-			<ul class="nav navbar-nav navbar-right">
-				@if (Auth::guest())
-					<li class="nav-item">
-						<a class="nav-link" href="{{ URL::route('login') }}">
-							{{ Lang::get('navbar.login_btn') }}
-						</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="{{ URL::route('signup') }}">
-							{{ Lang::get('navbar.signup_btn') }}
-						</a>
-					</li>
-				@else
-					<li class="dropdown">
-						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-						    <i class="fa fa-user"></i> {{ Auth::user()->name }} <span class="caret"></span>
-						</a>
-						<ul class="dropdown-menu" role="menu">
-							<li><a href="{{ URL::route('logout') }}">{{ Lang::get('navbar.logout_btn') }}</a></li>
-						</ul>
-					</li>
-				@endif
-			</ul>
-		</div>
+<nav>
+	<div class="nav-wrapper indigo darken-2">
+		<a class="brand-logo" href="{{ URL::route('home') }}">{{ $app_name }}</a>
+		<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+
+		<ul class="right hide-on-med-and-down">
+			@if (Auth::guest())
+				<li>
+					<a href="{{ URL::route('login') }}">
+						{{ Lang::get('navbar.login_btn') }}
+					</a>
+				</li>
+				<li>
+					<a href="{{ URL::route('signup') }}">
+						{{ Lang::get('navbar.signup_btn') }}
+					</a>
+				</li>
+			@else
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+						<i class="fa fa-user"></i>{{ Auth::user()->name }}<span class="caret"></span>
+					</a>
+					<ul class="dropdown-menu" role="menu">
+						<li>
+							<a href="{{ URL::route('logout') }}">
+								{{ Lang::get('navbar.logout_btn') }}
+							</a>
+						</li>
+					</ul>
+				</li>
+			@endif
+		</ul>
+
+		<ul class="side-nav" id="mobile-demo">
+			@if (Auth::guest())
+				<li>
+					<a href="{{ URL::route('login') }}">
+						{{ Lang::get('navbar.login_btn') }}
+					</a>
+				</li>
+				<li>
+					<a href="{{ URL::route('signup') }}">
+						{{ Lang::get('navbar.signup_btn') }}
+					</a>
+				</li>
+			@else
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li>
+							<a href="{{ URL::route('logout') }}">
+								{{ Lang::get('navbar.logout_btn') }}
+							</a>
+						</li>
+					</ul>
+				</li>
+			@endif
+		</ul>
 	</div>
 </nav>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,46 +1,58 @@
 <!DOCTYPE html>
-  <html>
+  <html lang="es">
     <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!--Let browser know website is optimized for mobile-->
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
       <!--Import Google Icon Font-->
       <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
       <!-- Font Awesome -->
       <script src="https://use.fontawesome.com/0f001b167c.js"></script>
 
       <!--Import materialize.css-->
-      <!-- Compiled and minified CSS -->
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/css/materialize.min.css">
 
-
-
-      <!--Let browser know website is optimized for mobile-->
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-
       <title>
-        @if (Route::currentRouteNamed('home'))
+        @if (Route::currentRouteNamed('welcome'))
           {{ $app_name }} | {{ $app_description }}
         @else
           {{ $title }} - {{ $app_name }}
         @endif
       </title>
+
+      <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+      <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+      <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
     </head>
 
     <body>
-      @include ('layouts._navbar_matcss')
+      @include ('layouts._navbar')
 
       @include ('layouts._alerts')
 
       @yield ('content')
 
 
+      <!-- SCRIPT -->
       <!--Import jQuery before materialize.js-->
+      <!--script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-rc1/jquery.min.js"></script-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <!-- Compiled and minified JavaScript -->
       <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.6/js/materialize.min.js"></script>
 
+      <!-- Inicializo la barra de navegación lateral para pantallas chicas -->
       <script type="text/javascript">
         $( document ).ready(function(){
           $(".button-collapse").sideNav();
         });
       </script>
+      <!-- /SCRIPT -->
+
     </body>
 </html>
