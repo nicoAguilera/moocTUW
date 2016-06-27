bd46def18cba0d02e631da0ff3ba687acd7494cb
Nicolas Aguilera <beltrusena@gmail.com>
2016-06-27 14:37:02 -0400

    Eliminando del repositorio el archivo vue.js

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -52,6 +52,11 @@
       <!-- Font Awesome -->
       <script src="https://use.fontawesome.com/0f001b167c.js"></script>
 
+      <!-- Vue versión de desarrollo -->
+      <!--script type="text/javascript" src="{{asset(js/vue.js)}}"></script-->
+
+      <!-- Vue versión de producción Incluir cdn al momento de mandar de hacer el deploy y eliminar la versión de desarrollo -->
+
       <!-- Inicializo la barra de navegación lateral para pantallas chicas -->
       <script type="text/javascript">
         $( document ).ready(function(){
